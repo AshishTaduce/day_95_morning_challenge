778f01d7a3d5910e2a0b0046e6bc09b8762ea2ca
AshishTaduce <ashishteddy2@gmail.com>
2020-03-09 10:17:51 +0530

    Challenge 2 done without recurse

--- bin/main.dart
+++ bin/main.dart
@@ -44,43 +44,40 @@
 import 'dart:math';
 
 void main() {
-  print(longestStreak(
-      [
-
-        {
-          "date": "2019-09-19"
-        },
-        {
-          "date": "2019-09-18"
-        },
-        {
-          "date": "2019-09-20"
-        },
-        {
-          "date": "2019-09-26"
-        },
-        {
-          "date": "2019-09-27"
-        },
-        {
-          "date": "2019-09-30"
-        }
-      ]
-  ));
-
-  print(longestStreak(
-      [
-        {
-          "date": "2019-09-30"
-        },
-        {
-        "date": "2019-10-01"
-        },
-        {
-          "date": "2019-10-02"
-        },
-      ]
-  ));
+//  print(longestStreak([ {
+//          "date": "2019-09-19"
+//        },
+//        {
+//          "date": "2019-09-18"
+//        },
+//        {
+//          "date": "2019-09-20"
+//        },
+//        {
+//          "date": "2019-09-26"
+//        },
+//        {
+//          "date": "2019-09-27"
+//        },
+//        {
+//          "date": "2019-09-30"
+//        }
+//      ]));
+//
+//  print(longestStreak([{
+//          "date": "2019-09-30"
+//        },
+//        {
+//        "date": "2019-10-01"
+//        },
+//        {
+//          "date": "2019-10-02"
+//        },
+//      ]));
+  print(reconstructString(['quick', 'brown', 'the', 'fox',], "thequickbrownfox"));
+  print(reconstructString(['bed', 'bath', 'bedbath', 'and', 'beyond',], "bedbathandbeyond"));
+  print(reconstructString(['bedbat', 'bed', 'bath', 'bedbath', 'and', 'beyond',], "bedbathandbeyond"));
+  print(reconstructString(["that", 'is', 'quick', 'brown', 'the', 'fox',], "thatisthequickbrownfox"));
 }
 
 int longestStreak(List<Map<String, String>> dates){
@@ -107,4 +104,19 @@ int longestStreak(List<Map<String, String>> dates){
 
   print(listOfStreaks);
   return listOfStreaks.isNotEmpty ? (listOfStreaks.reduce(max)) : 0;
+}
+
+String reconstructString(List<String> dictionary, String string){
+  String result = "";
+  for(int i = 0; i < string.length + 1; i ++){
+//    print('String is : $string and result is $result and index is $i');
+    if(dictionary.contains(string.substring(0, i))){
+      result = result + " " +string.substring(0, i);
+      string = string.substring(i);
+      i = -1;
+    }
+//      String temp = reconstructString(dictionary, string.substring(i + 1));
+//      if(temp != null)result = result + temp;
+  }
+  return string.isEmpty ? result : null;
 }
