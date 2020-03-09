259e41946c8eaccac71cc4391c609071262c9e4c
AshishTaduce <ashishteddy2@gmail.com>
2020-03-09 10:28:28 +0530

    Challenge 2 done without recurse, returning list of string

--- test/day_95_morning_challenge_test.dart
+++ test/day_95_morning_challenge_test.dart
@@ -1,9 +1,9 @@
 import 'package:test/test.dart';
 
 void main() {
-  test('String.split() splits the string on the delimiter', () {
-    var string = 'foo,bar,baz';
-    expect(string.split(','), equals(['foo', 'bar', 'baz']));
+  test('Test 1', () {
+    String string = 'bedbathandbeyond';
+    expect(reconstructString(), equals(['foo', 'bar', 'baz']));
   });
 
   test('String.trim() removes surrounding whitespace', () {
